db225b60439d1f59dce672d0d978904815110ede
Mx312275 <48981786+Mx312275@users.noreply.github.com>
2019-09-01 16:33:38 +0200

    Add files via upload

---
+++ install.py
@@ -0,0 +1,35 @@
+
+import os
+
+print ('\n[1] Termux\n[2] kali')
+
+x = input('\nEnter number > ')
+
+if x == '1':
+
+	with open('/data/data/com.termux/files/usr/etc/bash.bashrc','r')as y:
+		y = y.read()
+		X = y.find('alias style=\'python3 ~/.style.py\'')
+
+	if X != -1:
+		pass
+	else:
+		with open('/data/data/com.termux/files/usr/etc/bash.bashrc','a')as file:
+			file.write('alias style=\'python3 ~/.style.py\'')
+
+
+
+	if os.path.isfile('~/.style.py') == True:
+		print ('Restart the application please')
+		...
+	else:
+		os.system('git clone https://github.com/Mx312275/.style.py.git')
+		os.system('apt install figlet -y')
+		os.system('apt install toilet -y')
+		os.system('mv .style.py/.style.py ~/.style.py')
+		os.system('rm -rif .style.py')
+		os.system('clear')
+		print ('Restart the application please')
+
+else:
+	print('The tool will be downloaded to Kali soon')
